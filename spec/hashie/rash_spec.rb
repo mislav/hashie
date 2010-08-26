5a2121b3c51fab00626c02673061f58542faf0fe
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-08-26 13:38:34 +0200

    Rash: simplify implementation, delete unneeded specs

--- spec/hashie/rash_spec.rb
+++ spec/hashie/rash_spec.rb
@@ -1,28 +1,5 @@
 require 'spec_helper'
 
-describe String do
-  before do
-    @camel_case_strings = ["camelCase", "CamelCase", "stringTwoHumps", "StringHasThreeHumps"]
-    @underscore_strings = ["camel_case", "camel_case", "string_two_humps", "string_has_three_humps"]
-  end
-  
-  it "should convert camelcase strings to underscored strings" do
-    @camel_case_strings.collect{|s| s.to_underscore!}.should == @underscore_strings
-  end
-end
-
-describe Hash do  
-  before do
-    @hash1 = {"testOne" => "1", "TestTwo" => "2", :testThree => 3, :TestFour => 4, :testFiveSix => 56}
-    @hash1.rubyify_keys!
-    @hash2 = {"test_one" => "1", "test_two" => "2", "test_three" => 3, "test_four" => 4, "test_five_six" => 56}
-  end
-  
-  it "should convert camel case-ed keys to underscore-ed keys" do
-    @hash1.should == @hash2
-  end
-end
-
 describe Hashie::Rash do  
   before do 
     @original_hash = {
@@ -46,10 +23,6 @@ describe Hashie::Rash do
   
   it "should inherit from mash" do
     @rash.is_a?(Hashie::Mash).should be_true
-  end
-  
-  it "#initializing_reader should return a Hashie::Rash when passed a non-existent key" do
-    @rash.initializing_reader(:abc).is_a?(Hashie::Rash).should be_true
   end
   
   it "should create a new rash where all the keys are underscored instead of camelcased" do
